7a0e4c4edc00b612b670c68db26bb74b169382c8
Andrew <andrewmoren93@gmail.com>
2023-11-29 22:44:19 +0300

    add components

---
+++ src/components/Track/Track.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import style from "./track.module.scss";
+
+const Track = (track) => {
+  return <div>{JSON.stringify(track)}</div>;
+};
+
+export default Track;
--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,8 +1,20 @@
 import React from "react";
+
 import trackList from "../../assets/trackList.js";
+import Track from "../../components/Track/Track.jsx";
+import style from "./mainPage.module.scss";
 
 const MainPage = () => {
-  return <div>Main Page</div>;
+  return (
+    <div className={style.search}>
+      <>Поиск треков</>
+      <div className={style.list}>
+        {trackList.map((track) => (
+          <Track key={track.id} {...track} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default MainPage;
