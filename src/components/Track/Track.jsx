ed3fe9be31b667dacfac2e143b31e09efdedef29
Andrew <andrewmoren93@gmail.com>
2023-12-01 21:44:41 +0300

    add format mm ss

--- src/components/Track/Track.jsx
+++ src/components/Track/Track.jsx
@@ -3,9 +3,13 @@ import React from "react";
 import style from "./track.module.scss";
 import { IconButton } from "@mui/material";
 import { PlayArrow } from "@mui/icons-material";
+import secondsToMMSS from "../../utils/secondsToMMSS";
 
 const Track = (track) => {
   const { id, src, preview, title, artists, duration } = track;
+
+  const formattedDuration = secondsToMMSS(duration);
+
   return (
     <div className={style.track}>
       <IconButton>
@@ -16,7 +20,7 @@ const Track = (track) => {
         <b>{title}</b>
         <p>{artists}</p>
       </div>
-      <p>{duration}</p>
+      <p>{formattedDuration}</p>
     </div>
   );
 };
