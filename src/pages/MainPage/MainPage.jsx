8df97ff9d954617b84179e70311371a51e4bb4ea
Andrew <andrewmoren93@gmail.com>
2023-12-01 21:49:48 +0300

    realize search

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -3,11 +3,20 @@ import React from "react";
 import trackList from "../../assets/trackList.js";
 import Track from "../../components/Track/Track.jsx";
 import style from "./mainPage.module.scss";
+import { Input } from "@mui/material";
 
 const MainPage = () => {
+  const handleChange = (event) => {
+    console.log(event.target.value);
+  };
+
   return (
     <div className={style.search}>
-      <>Поиск треков</>
+      <Input
+        className={style.input}
+        placeholder="Поиск треков"
+        onChange={handleChange}
+      />
       <div className={style.list}>
         {trackList.map((track) => (
           <Track key={track.id} {...track} />
