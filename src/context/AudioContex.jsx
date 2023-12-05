de10742a1db0e50e3877413ba1e6bbd1fa3023e6
Andrew <andrewmoren93@gmail.com>
2023-12-05 23:47:49 +0300

    add context1

--- src/context/AudioContex.jsx
+++ src/context/AudioContex.jsx
@@ -1,4 +1,10 @@
 import React from "react";
-import { createContext } from "react";
+import { createContext, useState } from "react";
+import trackList from "../assets/trackList";
 
-const AudioContex = createContext({});
+export const AudioContex = createContext({});
+
+const AudioProvider = () => {
+  const [currentTrack, setCurrentTrack] = useState(trackList[0]);
+  const [isPlaying, setIsPlaying] = useState(false);
+};
