5d32f9dd1ed34cf8b1040d98da72119d0c8da6c6
Andrew <andrewmoren93@gmail.com>
2023-12-06 01:10:32 +0300

    add handleToggleAudio

--- src/context/AudioContex.jsx
+++ src/context/AudioContex.jsx
@@ -4,7 +4,14 @@ import trackList from "../assets/trackList";
 
 export const AudioContex = createContext({});
 
-const AudioProvider = () => {
+const AudioProvider = ({ children }) => {
   const [currentTrack, setCurrentTrack] = useState(trackList[0]);
   const [isPlaying, setIsPlaying] = useState(false);
+
+  const handleToggleAudio = (track) => {
+    console.log("click!");
+  };
+  return <AudioContext.Provider value={{}}>{children}</AudioContext.Provider>;
 };
+
+export default AudioProvider;
