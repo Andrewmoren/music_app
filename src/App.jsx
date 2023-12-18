50f07fb5f22d83cc09afe3c53aea44f97722e682
Andrew <andrewmoren93@gmail.com>
2023-12-18 23:46:08 +0300

    playbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from "react";
-
+import Playbar from "./components/Playbar/Playbar";
 import MainPage from "./pages/MainPage/MainPage";
 import style from "./global.module.scss";
 
 const App = () => (
   <div className={style.wrapper}>
     <MainPage />
+    <Playbar />
   </div>
 );
 
--- src/components/Playbar/Playbar.jsx
+++ src/components/Playbar/Playbar.jsx
@@ -1,3 +1,29 @@
-const Playbar = () => <></>;
+import { useContext } from "react";
+import { AudioContex } from "../../context/AudioContex";
+import style from "./playbar.module.scss";
+import { Slider, IconButton } from "@mui/material";
+import { PlayArrow, Pause } from "@mui/icons-material";
+
+const Playbar = () => {
+  const { audio, currentTrack, handleToggleAudio, isPlaying } =
+    useContext(AudioContex);
+
+  const { title, artists, preview, duration } = currentTrack;
+  return (
+    <div className={style.playbar}>
+      <img className={style.preview} src={preview} alt="" />
+      <IconButton onClick={() => handleToggleAudio(currentTrack)}>
+        {isPlaying ? <Pause /> : <PlayArrow />}
+      </IconButton>
+      <div className={style.credits}>
+        <h4>{title}</h4>
+        <p>{artists}</p>
+      </div>
+      <div className={style.slider}>
+        <p>00:00</p>
+      </div>
+    </div>
+  );
+};
 
 export default Playbar;
