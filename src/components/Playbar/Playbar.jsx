8b7e8f4407e1ba9d3c4789c449ba38b833d7244b
Andrew <andrewmoren93@gmail.com>
2023-12-18 23:53:04 +0300

    time duration

--- src/components/Playbar/Playbar.jsx
+++ src/components/Playbar/Playbar.jsx
@@ -3,12 +3,15 @@ import { AudioContex } from "../../context/AudioContex";
 import style from "./playbar.module.scss";
 import { Slider, IconButton } from "@mui/material";
 import { PlayArrow, Pause } from "@mui/icons-material";
+import secondsToMMSS from "../../utils/secondsToMMSS";
 
 const Playbar = () => {
   const { audio, currentTrack, handleToggleAudio, isPlaying } =
     useContext(AudioContex);
 
   const { title, artists, preview, duration } = currentTrack;
+
+  const formattedDuration = secondsToMMSS(duration);
   return (
     <div className={style.playbar}>
       <img className={style.preview} src={preview} alt="" />
@@ -21,6 +24,8 @@ const Playbar = () => {
       </div>
       <div className={style.slider}>
         <p>00:00</p>
+        <Slider step={1} min={0} max={100} />
+        <p>{formattedDuration}</p>
       </div>
     </div>
   );
